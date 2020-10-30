6ea1be396e7dd4f10e25d617f586f04f3cf30478
Harshit Pant <pantharshit00@gmail.com>
2020-10-30 20:13:10 +0530

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Introduction
+
+Reproduction for https://github.com/prisma/prisma/issues/3985
+
+## To reproduce
+
+1. Clone this reproduce and replace creds in .env
+2. Run db push to get a database from the schema 
+3. Run `yarn start` and inspect the generated query
+
---
+++ main.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+
+async function main() {
+  const prisma = new PrismaClient({
+    log: ["query"],
+  });
+  const { count } = await prisma.payslip.updateMany({
+    where: {
+      AND: [
+        { employeeId: { in: [""] } },
+        { status: "uploaded" },
+        { paymentDate: new Date().toISOString() },
+        { publishedAt: null },
+      ],
+    },
+    data: {
+      status: "published",
+      publishedAt: new Date(),
+      updatedAt: new Date(),
+    },
+  });
+
+  console.log(count);
+  prisma.$disconnect();
+}
+
+main();
